421be6e3240d3edc83b2386bb9ddd9f171875789
AswathyAchii <aswathyng1041998@gmail.com>
2024-09-23 15:03:47 +0400

    reverting the changes back

--- lib/src/uaepass_login_view.dart
+++ lib/src/uaepass_login_view.dart
@@ -1,10 +1,10 @@
 import 'dart:developer';
-import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:url_launcher/url_launcher_string.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
 import '../uaepass.dart';
 import 'configuration.dart';
@@ -20,13 +20,27 @@ class UaepassLoginView extends StatefulWidget {
 class _UaepassLoginViewState extends State<UaepassLoginView> {
   double progress = 0;
   String successUrl = '';
-  // InAppWebViewController? webViewController;
-  // PullToRefreshController? pullToRefreshController;
+  InAppWebViewController? webViewController;
+  PullToRefreshController? pullToRefreshController;
   final MethodChannel channel = const MethodChannel('poc.uaepass/channel1');
-  WebViewController? _controller;
+
   @override
   void initState() {
     super.initState();
+    channel.setMethodCallHandler((MethodCall call) async {
+      final decoded = Uri.decodeFull(successUrl);
+      if (!kIsWeb && defaultTargetPlatform == TargetPlatform.android) {
+        await InAppWebViewController.setWebContentsDebuggingEnabled(kDebugMode);
+      }
+
+      webViewController?.loadUrl(urlRequest: URLRequest(url: WebUri(decoded)));
+    });
+  }
+
+  @override
+  void dispose() {
+    webViewController?.dispose();
+    super.dispose();
   }
 
   @override
@@ -37,92 +51,15 @@ class _UaepassLoginViewState extends State<UaepassLoginView> {
         if (!snapshot.hasData) {
           return const CircularProgressIndicator();
         }
-        // _controller?.loadRequest(Uri.parse(snapshot.data!));
         return Scaffold(
-            // appBar: AppBar(
-            //   backgroundColor: const Color(0xFF55C9B2),
-            //   foregroundColor: Colors.black,
-            //   title: const Text('UAE Pass'),
-            //   automaticallyImplyLeading: false,
-            // ),
-            body: SafeArea(
-          child: WebViewWidget(
-              controller: WebViewController()
-                ..clearCache()
-                ..enableZoom(false)
-                ..setJavaScriptMode(JavaScriptMode.unrestricted)
-                ..setNavigationDelegate(
-                  NavigationDelegate(
-                    onProgress: (int progress) {},
-                    onPageStarted: (String endUrl) {
-                      final cookieManager = WebViewCookieManager();
-
-                      cookieManager.clearCookies();
-                      final uri = Uri.dataFromString(endUrl);
-                      if (endUrl.contains('error=')) {
-                        switch (uri.queryParameters['error'].toString()) {
-                          case "cancelled":
-                            return Navigator.of(context).pop();
-                          case "cancelledOnApp":
-                            return Navigator.of(context).pop();
-                        }
-                        return Navigator.of(context).pop();
-                      }
-                      if (endUrl.contains('code=')) {
-                        // state = uri.queryParameters['state']!;
-                        final code = uri.queryParameters['code']!;
-
-                        return Navigator.of(context).pop(code);
-                      } else if (endUrl == "https://selfcare.uaepass.ae/" ||
-                          endUrl.contains("https://ercweb.mvp-apps.ae/auth")) {
-                        return Navigator.of(context).pop();
-                      }
-                    },
-                    onPageFinished: (String url) {},
-                    onHttpError: (HttpResponseError error) {},
-                    onWebResourceError: (error) {},
-                    onNavigationRequest: (NavigationRequest request) async {
-                      final url = request.url.toString();
-                      if (Configuration.app2App &&
-                          (url.contains('uaepass://') ||
-                              url.contains('uaepass.ae/'))) {
-                        final openUrl =
-                            await Helper.getUaePassOpenUrl(Uri.parse(url));
-                        successUrl = openUrl.successUrl;
-                        // print('success: $successUrl');
-                        // print('oepnUrl: ${openUrl.appUrl}');
-
-                        await launchUrlString(openUrl.appUrl);
-                        return NavigationDecision.navigate;
-                      }
-
-                      if (url.contains('code=')) {
-                        final code = Uri.parse(url).queryParameters['code'];
-                        Navigator.pop(context, code);
-                      } else if (url.contains('cancelled')) {
-                        if (Uaepass.instance.showMessages) {
-                          log('User cancelled login with UAE Pass');
-                          // ScaffoldMessenger.of(context)
-                          //   ..removeCurrentSnackBar()
-                          //   ..showSnackBar(
-                          //     const SnackBar(
-                          //       content: Text('User cancelled login with UAE Pass'),
-                          //     ),
-                          //   );
-                        }
-                        Navigator.pop(context);
-                      }
-                      return NavigationDecision.navigate;
-                    },
-                  ),
-                )
-                ..loadRequest(
-                  Uri.parse(
-                    usePassBaseUrl,
-                  ),
-                )
-
-              /*InAppWebView(
+          // appBar: AppBar(
+          //   backgroundColor: const Color(0xFF55C9B2),
+          //   foregroundColor: Colors.black,
+          //   title: const Text('UAE Pass'),
+          //   automaticallyImplyLeading: false,
+          // ),
+          body: SafeArea(
+            child: InAppWebView(
               initialUrlRequest: URLRequest(url: WebUri(snapshot.data!)),
               initialSettings: InAppWebViewSettings(
                 supportZoom: false,
@@ -138,7 +75,8 @@ class _UaepassLoginViewState extends State<UaepassLoginView> {
                 if (Configuration.app2App &&
                     (url.contains('uaepass://') ||
                         url.contains('uaepass.ae/'))) {
-                  final openUrl = Helper.getUaePassOpenUrl(uri.request.url!);
+                  final openUrl =
+                      await Helper.getUaePassOpenUrl(uri.request.url!);
                   successUrl = openUrl.successUrl;
                   // print('success: $successUrl');
                   // print('oepnUrl: ${openUrl.appUrl}');
@@ -166,17 +104,9 @@ class _UaepassLoginViewState extends State<UaepassLoginView> {
                 return null;
               },
             ),
-            */
-              ),
-        ));
+          ),
+        );
       },
     );
   }
-}
-
-String get usePassBaseUrl {
-  // if (server == Build.testing) {
-  //   return "https://stg-id.uaepass.ae/idshub/authorize?response_type=code&client_id=sandbox_stage&scope=urn:uae:digitalid:profile:general&state=https://emiratesrc.ae&redirect_uri=https://www.emiratesrc.ae/social_sup/Social_sup.asmx/uae_pass_redirect&acr_values=urn:safelayer:tws:policies:authentication:level:low";
-  // }
-  return "https://id.uaepass.ae/idshub/authorize?response_type=code&client_id=rcuae_web_prod&scope=urn:uae:digitalid:profile:general&state=https://emiratesrc.ae&redirect_uri=http://www.emiratesrc.ae/social_sup/Social_sup.asmx/uae_pass_redirect&acr_values=urn:safelayer:tws:policies:authentication:level:low";
 }
